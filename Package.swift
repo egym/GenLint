b2e029dd91ca4fc844e36e2304e94ab4dec470f2
Puneet Sharma <puneet.sharma@egym.com>
2024-01-11 10:02:52 +0100

    Remove SwiftGen

--- Package.swift
+++ Package.swift
@@ -5,10 +5,6 @@ let package = Package(
     name: "GenLint Plugin Package",
     products: [
         .plugin(
-            name: "SwiftGenPlugin",
-            targets: ["SwiftGenPlugin"]
-        ),
-        .plugin(
             name: "SwiftLintPlugin",
             targets: ["SwiftLintPlugin"]
         ),
@@ -16,22 +12,11 @@ let package = Package(
     targets: [
         // Binaries
         .binaryTarget(
-            name: "SwiftGenBinary",
-            path: "Binaries/SwiftGenBinary.artifactbundle"
-        ),
-        .binaryTarget(
             name: "SwiftLintBinary",
             path: "Binaries/SwiftLintBinary.artifactbundle"
         ),
 
         // Plugins
-        .plugin(
-            name: "SwiftGenPlugin",
-            capability: .buildTool(),
-            dependencies: [
-                "SwiftGenBinary"
-            ]
-        ),
         .plugin(
             name: "SwiftLintPlugin",
             capability: .buildTool(),
